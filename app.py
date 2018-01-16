3452a0aa5c9e5e89ee2d2aa5c469c4cb2c3ff08c
Artyom K <hotkosc@gmail.com>
2018-01-16 20:40:05 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+
+from django_micro import configure, route, run, get_app_label
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import models
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
+configure(locals())
+
+
+class Quote(models.Model):
+    content = models.TextField()
+
+    class Meta:
+        app_label = get_app_label()
+
+
+@route('')
+def homepage(request):
+    quotes = Quote.objects.all()
+    return render(request, 'base.html', {'quotes': quotes})
+
+
+application = run()
